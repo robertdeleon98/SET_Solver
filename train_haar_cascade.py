922342bff8d3ec009213c3f7f9facf0402f4630b
robertdeleon98 <robertdeleon98@gmail.com>
2021-06-13 17:28:54 -0700

    added training file

--- train_haar_cascade.py
+++ train_haar_cascade.py
@@ -1,1 +1,4 @@
-import os
+# === IMPORT NEGATIVE IMAGES FOR TRAINING (RUN ONCE) ===
+# import kaggle
+# kaggle.api.authenticate()
+# kaggle.api.dataset_download_files('muhammadkhalid/negative-images', path='image-data/negative-images', unzip=True)
