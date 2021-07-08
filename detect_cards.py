1b168b228899be4a80b9ae2ea01a159b18eaf6a1
robertdeleon98 <robertdeleon98@gmail.com>
2021-07-08 13:05:39 -0700

    added detect_cards.py

---
+++ detect_cards.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+card_cascade = cv2.CascadeClassifier('set_card_cascade_v1.xml')
+
+img = cv2.imread('image-data/setgametest.jpg')
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cards = card_cascade.detectMultiScale(gray_img, scaleFactor=1.04, minNeighbors=9, minSize=(30, 30))
+
+for (x, y, w, h) in cards:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, 'card', (x+w, y+h), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+
+cv2.imshow('test', img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
