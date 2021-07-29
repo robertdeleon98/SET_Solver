3fd2c1cd0ca4720628e1d6996ee8c20756888608
NKonganda <55722447+NKonganda@users.noreply.github.com>
2021-07-29 13:08:58 -0700

    synchronising code

--- detect_cards.py
+++ detect_cards.py
@@ -1,13 +1,27 @@
 import numpy as np
 import cv2
 import os
+import math
 
 card_cascade = cv2.CascadeClassifier('Cascades/set_card_cascade_v3.xml')
 dir = "Temp_Images"
-img = cv2.imread('Test_Images/test6.jpg')
+img = cv2.imread('Test_Images/test4.jpg')
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cards = card_cascade.detectMultiScale(gray_img, scaleFactor=1.04, minNeighbors=4, minSize=(30, 30))
+
+if not os.path.exists('Temp_Images'):
+    os.makedirs('Temp_Images')
+
+# def face_distance_to_conf(face_distance, face_match_threshold=0.6):
+#     if face_distance > face_match_threshold:
+#         range = (1.0 - face_match_threshold)
+#         linear_val = (1.0 - face_distance) / (range * 2.0)
+#         return linear_val
+#     else:
+#         range = face_match_threshold
+#         linear_val = 1.0 - (face_distance / (range * 2.0))
+#         return linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))
 
 for (x, y, w, h) in cards:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
