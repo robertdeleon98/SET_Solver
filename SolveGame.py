8ab506616ec2720ebe2a40d65109e92879815e4f
robertdeleon98 <robertdeleon98@gmail.com>
2021-08-12 16:39:47 -0400

    updated file

---
+++ SolveGame.py
@@ -0,0 +1,7 @@
+'''
+SET Rules
+They all have the same number or have three different numbers.
+They all have the same shape or have three different shapes.
+They all have the same shading or have three different shadings.
+They all have the same color or have three different colors.
+'''
