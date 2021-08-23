063e5b693ac5ea99305905633e4e26b52a818c29
robertdeleon98 <robertdeleon98@gmail.com>
2021-08-22 22:36:58 -0400

    updated solvegame.py

--- SolveGame.py
+++ SolveGame.py
@@ -1,32 +1,75 @@
 '''
 SET Rules
-They all have the same number or have three different numbers.
-They all have the same shape or have three different shapes.
-They all have the same shading or have three different shadings.
 They all have the same color or have three different colors.
+They all have the same shading or have three different shadings.
+They all have the same shape or have three different shapes.
+They all have the same number or have three different numbers.
 '''
 
-import numpy as np
-import pandas as pd
-import cv2
-import os
-from random import shuffle
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import imread, imshow, subplots, show
 
-number = ['one', 'two', 'three']
-shape = ['oval', 'diamond', 'squiggly']
-shading = ['full', 'striped', 'empty']
-color = ['red', 'green', 'purple']
+# import numpy as np
+# import pandas as pd
+# import cv2
+# import os
+# from random import shuffle
+# from tqdm import tqdm
+# import matplotlib.pyplot as plt
+# from matplotlib.pyplot import imread, imshow, subplots, show
 
 
+class Card:
+    def __init__(self, color, shading, shape, number):
+        self.color = color
+        self.shading = shading
+        self.shape = shape
+        self.number = number
 
 
-image = imread('image-data/positive-images/r-f-s-2.PNG')
-images = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-imshow(images[0])
-show()
+card1 = Card('', '', '', '')
+filename = "p-e-d-2.PNG"
+
+i = 0
+while filename[i] != '.':
+    if filename[i] == 'r':
+        card1.color = 'red'
+    elif filename[i] == 'g':
+        card1.color = 'green'
+    elif filename[i] == 'p':
+        card1.color = 'purple'
+    elif filename[i] == 'f':
+        card1.shading = 'full'
+    elif filename[i] == 'l':
+        card1.shading = 'lines'
+    elif filename[i] == 'e':
+        card1.shading = 'empty'
+    elif filename[i] == 'o':
+        card1.shape = 'oval'
+    elif filename[i] == 'd':
+        card1.shape = 'diamond'
+    elif filename[i] == 's':
+        card1.shape = 'squiggly'
+    elif filename[i] == '1':
+        card1.number = 'one'
+    elif filename[i] == '2':
+        card1.number = 'two'
+    elif filename[i] == '3':
+        card1.number = 'three'
+    i += 1
+
+print(card1.color, card1.shading, card1.shape, card1.number)
+
+# number = ['one', 'two', 'three']
+# shape = ['oval', 'diamond', 'squiggly']
+# shading = ['full', 'lines', 'empty']
+# color = ['red', 'green', 'purple']
+#
+#
+#
+#
+# image = imread('image-data/positive-images/r-f-s-2.PNG')
+# images = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+# imshow(images[0])
+# show()
 
 '''
 read image name and extract features from image file name
